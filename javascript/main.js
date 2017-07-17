daa35fee2c224c47e186380c97ae01a311eab6d7
Daniel Greene <d.b.greene101@gmail.com>
2017-07-17 14:15:10 -0500

    modified image names

--- javascript/main.js
+++ javascript/main.js
@@ -5,43 +5,43 @@ let watch = [
     name: 'The Gentleman',
     description: 'The Gentleman is a classic watched based of ancient Egyptian designs. Its sleek look and wide appeal means you can check the time, anytime. ',
     price: "$196",
-    image:"images/prod-watch1.jpg"
+    image:"images/prod-watch1.png"
   },
   {
     name: 'The Lord',
     description: 'The Lord is a watch with a royal appeal. Sculpted to be reminiscent of Odin’s remaining eye, The Lord is entirely unique in its look and appeal. ',
     price: "$188",
-    image:"images/prod-watch2.jpg"
+    image:"images/prod-watch2.png"
   },
   {
     name: 'The One',
     description: 'The One was built to remind one of the feeling of watching their favorite action movie. Idk this text doesn’t really matter and it’s filler. ',
     price: "$176",
-    image:"images/prod-watch3.jpg"
+    image:"images/prod-watch3.png"
   },
   {
     name: 'Smart Time',
     description: 'It is a smart-watch that does smart things like invent the Tesla car, and predict the future. ',
     price: "$166",
-    image:"images/prod.watch4.jpg"
+    image:"images/prod-watch4.png"
   },
   {
     name: 'The Leatherman',
     description: 'A watch with leather that looks mediocre at best. This filler text is brought to you by two cups of coffee and a mild hangover. ',
     price: "$155",
-    image:"images/prod-watch5.jpg"
+    image:"images/prod-watch5.png"
   },
   {
     name: 'Silver Streak',
     description: 'Have you seen the movie Silver Streak with Richard Pryer and Gene Wilder? It’s really good and worth your time. ',
     price: "$144",
-    image:"images/prod-watch6.jpg"
+    image:"images/prod-watch6.png"
   },
   {
     name: 'Apple Watch',
     description: 'We stole it and now we sell it. It’s ours, try and sue us. ',
     price: "$133",
-    image:"images/prod-watch7.jpg"
+    image:"images/prod-watch7.png"
   },
   {
     name: 'The Idea',
