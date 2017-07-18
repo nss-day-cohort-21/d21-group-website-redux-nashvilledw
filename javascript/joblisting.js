bf5cf9e9a2cd202b2c98aea64618d49a5dcfe51f
Daniel Greene <d.b.greene101@gmail.com>
2017-07-18 09:50:25 -0500

    monster

--- javascript/joblisting.js
+++ javascript/joblisting.js
@@ -9,24 +9,24 @@ PAGE: JOBLISTING
 let jobs = [
 {
   name: "Employee Character Artist",
-  description: "Draw our employees.   Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt." 
+  description: "Draw our employees.  <br> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt." 
 },
 {
   name: "Watch Designer",
-  description: "Make mad-dope watches.   Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt." 
+  description: "Make mad-dope watches.  <br> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt." 
 },
 {
   name: "Marketing Guru",
-  description: "Get people to buy our stuff.  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt." 
+  description: "Get people to buy our stuff. <br> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt." 
 },
 {
   name: "Watch Tinker",
-  description: "Tinker, Tinker, Tinker.   Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt." 
+  description: "Tinker, Tinker, Tinker.  <br> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt." 
 }
 ];
 
 jobs.forEach(function(item){
-  document.getElementById("openJobs").innerHTML += `<li><h2 class="watchHeader">${item.name}</h2></li> <ul><li class="jobDescript">${item.description}</li></ul> <ul><li class="applybox"><a href="apply.html">Click To Apply</a></li></ul>`
+  document.getElementById("openJobs").innerHTML += `<li><h2 class="watchHeader">${item.name}</h2></li> <ul><li class="jobDescript">${item.description}</li></ul> <ul><li class="applybox"><a href="https://www.monster.com/">Click To Apply</a></li></ul>`
 });
 
 
